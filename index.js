b1c92d016ed6f33f7c7c3d2e56e3049205fb0ba2
oscar0812 <oscar0812torres@gmail.com>
2023-04-28 20:45:21 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import TypescriptNodeUtil from "./typescript-node-util.js";
+const args = process.argv;
+
+let file_path = args.length > 2 ? args[2] : '';
+
+let nodeUtil = new TypescriptNodeUtil(file_path);
